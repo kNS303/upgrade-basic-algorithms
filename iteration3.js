b96775a6d33a0ee4c107265c8241ac900bc8a587
Alvaro Torres <kanas89@gmail.com>
2021-10-01 17:46:11 +0200

    third commit

---
+++ iteration3.js
@@ -0,0 +1,25 @@
+let resultado1 = (10 * 5);
+
+console.log (restado1)
+
+let resultado2 = 10 / 5;
+
+console.log (resultado2)
+
+let resultado3 = 15 / 9;
+
+console.log (resultado3)
+
+let y = 10;
+let z = 5;
+
+let x = (y + z); 
+
+console.log (x)
+
+let y = 10;
+let z = 5;
+
+let x = (10 * 5)
+
+console.log (x)
