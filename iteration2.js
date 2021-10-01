136f43b28db6a44a0a37f3a4370740e1d6f01672
Alvaro Torres <kanas89@gmail.com>
2021-10-01 17:23:05 +0200

    segundo commit

---
+++ iteration2.js
@@ -0,0 +1,25 @@
+const character = {name: 'Jack Sparrow', age: 10};
+age = 25
+
+let edad = age
+
+console.log (edad)
+
+/////////
+
+let firstName = "Jon";
+let lastName = "snow";
+let age = 24;
+
+let frase = "Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos."
+
+console.log (frase)
+
+/////////
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+let precioFinal = toy1.price + toy2.price 
+
+console.log (precioFinal)
